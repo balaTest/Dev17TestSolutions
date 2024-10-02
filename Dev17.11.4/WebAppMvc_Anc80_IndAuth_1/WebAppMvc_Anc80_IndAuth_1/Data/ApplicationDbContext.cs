873f9fa0150216df4715e44b788b9907bc2cf373
Bala Chirtsabesan <balach@microsoft.com>
2024-10-02 18:08:28 +0530

    [WebAppMvc_Anc80_IndAuth_1] Created solution

---
+++ Dev17.11.4/WebAppMvc_Anc80_IndAuth_1/WebAppMvc_Anc80_IndAuth_1/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebAppMvc_Anc80_IndAuth_1.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
